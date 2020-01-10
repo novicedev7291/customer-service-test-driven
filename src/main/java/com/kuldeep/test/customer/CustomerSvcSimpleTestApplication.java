c5ab2108de234d5d8cc11becd150d28a26cc8893
kuldeep <kuldeepyadav7291@gmail.com>
2020-01-10 18:59:35 +0530

    Add initial db value initializer

--- src/main/java/com/kuldeep/test/customer/CustomerSvcSimpleTestApplication.java
+++ src/main/java/com/kuldeep/test/customer/CustomerSvcSimpleTestApplication.java
@@ -1,10 +1,26 @@
 package com.kuldeep.test.customer;
 
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.stream.Stream;
 
 @SpringBootApplication
 public class CustomerSvcSimpleTestApplication {
+
+
+	@Bean
+	ApplicationRunner init(final CustomerRepository customerRepository){
+		return args -> {
+			Stream.of("a", "b", "c").
+					forEach(
+						n -> customerRepository.saveAndFlush(new Customer(null, n, n, n + "@email.com"))
+					);
+		};
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(CustomerSvcSimpleTestApplication.class, args);
